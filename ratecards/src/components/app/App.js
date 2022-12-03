e3a83a405aeea921ef3f155f792c2204b8ad292b
Ekaterina Kotenko <104442215+FrauReise@users.noreply.github.com>
2022-12-03 11:53:28 +0300

    corrected json

--- ratecards/src/components/app/App.js
+++ ratecards/src/components/app/App.js
@@ -1,5 +1,22 @@
 import './App.css';
-import Rate from "./components/rate/Rate";
+import style from './style/Rate.scss'
+import Rate from './components/rate/Rate';
+import data from '../../data/data.json';
 
+
+
+let rates = JSON.parse(rateJSON);
+    
+function App() {
+  return (
+    <div className="App">
+      {
+        rates.map((rate) =>
+          <Tarif tarif={tarif.tarif} speed={rate.speed} currency={rate.currency} period={rate.period} classHeader={rate.classHeader} classBody={rate.classBody} classTarif={rate.classTarif}></Tarif>
+          )
+        }
+    </div>
+  );
+}
 
 export default App;
